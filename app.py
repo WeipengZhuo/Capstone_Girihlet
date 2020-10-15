4c06dbcdbdf71a0f8e96ce5a3d88ac09988a1452
weipeng zhuo <wzhuo7@gmail.com>
2020-10-15 01:37:38 -0400

    Working Template

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 import os
-from flask import Flask, jsonify, render_template
-import database
-import commands
+from flask import Flask, jsonify, render_template, request, redirect
+
+from sqlalchemy import func
+# import database
+# import commands
+# from model import Model
 from config import DevelopmentConfig, ProductionConfig
-from model import Model
+# import config
+TEMPLATE_DIR = os.path.abspath('./templates')
+STATIC_DIR = os.path.abspath('./static')
+
 
 # init flask app instance
-app = Flask(__name__)
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
 # setup with the configuration provided by the user / environment
 # app.config.from_object(os.environ['APP_SETTINGS'])
 # app.config.from_envvar('APP_SETTINGS')
-
-app.config.from_object(ProductionConfig)
+app.config.from_object(DevelopmentConfig)
 
 # setup all our dependencies, for now only database using application factory pattern
-database.init_app(app)
-commands.init_app(app)
+# database.init_app(app)
+# commands.init_app(app)
 
 
 @app.route("/")
 def main_page():
-    items = Model.query.all()
-    return render_template('index2.html', items=items)
-
-
-@app.route("/add/<string:item>", methods=['POST'])
-def add_new_item(item):
-    model = Model(parameter=item)
-
-    # add to the database session
-    database.db.session.add(model)
-
-    # commit to persist into the database
-    database.db.session.commit()
-
-    return jsonify({"success": model.parameter})
-
+    # items = Model.query.all()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -1,19 +1,18 @@
 import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SECRET_KEY = 'b\\'\\xc5>`\\xe3C\\x19\\x13\\xdc\\xeaV\\xefT\\x9d\\xa4x\\xae\\''
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # SECRET_KEY = 'b\\'\\xc5>`\\xe3C\\x19\\x13\\xdc\\xeaV\\xefT\\x9d\\xa4x\\xae\\''
 
 
@@ -22,8 +21,8 @@ class DevelopmentConfig(Config):
     DEVELOPMENT = True
     # SECRET_KEY = 'b\\'\\xc5>`\\xe3C\\x19\\x13\\xdc\\xeaV\\xefT\\x9d\\xa4x\\xae\\''
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///local_database.db'
-
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///local_database.db'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(Config):
     TESTING = True
